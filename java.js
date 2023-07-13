6f84386d19866a169c8b51aa11583492f3a885a2
kenser <koffy140221@gmail.com>
2023-07-12 20:20:19 -0600

    agregar el uso del evento DOMContentLoaded

---
+++ java.js
@@ -0,0 +1,9 @@
+// Hacer el  uso del evento DOMContentLoaded para asegurarse de que todos los elementos del documento estén disponibles antes de ejecutar el código.
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.getElementById('formul');
+    const inputsContainer = document.getElementById('input');
+    const addInputBtn = document.getElementById('agregar');
+    const removeInputBtn = document.getElementById('eliminar');
+
+    let inputCount = 0;
+});
