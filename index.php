f79c97f579c52f03f21f9460717ccac41e4c4d5f
kenser <koffy140221@gmail.com>
2023-07-12 20:00:10 -0600

    crear un div para el formulario dinamico

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TAREA NUMERO 4 "FORMULARIO DINAMICO</title>
+</head>
+<body>
+    <div class="container">
+        <h1>FORMULARIO DINAMICO</h1>
+        <form id="formul" action="#" method="POST">
+            <div id="input"></div>
+            <button id="agregar" class="btn btn-primary" type="button">AGREGAR</button>
+            <button id="eliminar" class="btn btn-secondary" type="button">ELIMINAR</button>
+            <br><br>
+            <button id="enviar" class="btn btn-success" type="submit">ENVIAR</button>
+        </form>
+    </div>
+    <script src="java.js"></script>
+</body>
+</html>
